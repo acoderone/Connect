b32fce5e0830487b37d406aaddc9fc4c2c2167bc
Acoderone <avi5200786@gmail.com>
2024-07-09 05:15:37 +0530

    sockets

--- Client/Connect/src/Components/User.jsx
+++ Client/Connect/src/Components/User.jsx
@@ -2,39 +2,43 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import {jwtDecode} from "jwt-decode";
 
-import {jwtDecode} from 'jwt-decode';
 const socket = io("http://localhost:3000", {
   withCredentials: true,
 });
-//const usernameFromCookie = Cookies.get('user');
+
 function User() {
-  const [user, setUser] = useState(null); // Initialize as null
-  const { userId } = useParams(); // Get userId from route parameters
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
   const [username, setUsername] = useState("");
-  const [sender,setSender]=useState("");
+  const [sender, setSender] = useState("");
+
   useEffect(() => {
-    // Read username from cookies
     socket.connect();
-    const token = localStorage.getItem('token'); // Adjust this based on how you store your token
-    //console.log(token);
+
+    const token = localStorage.getItem("token");
     if (token) {
       const decodedToken = jwtDecode(token);
       setSender(decodedToken.username);
     }
-    //console.log(usernameFromCookie);
 
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://localhost:3000/users/${userId}`, { withCredentials: true });
-        
+        const response = await axios.get(
+          `http://localhost:3000/users/${userId}`,
+          {
+            withCredentials: true,
+          }
+        );
+        console.log(response);
         setUser(response.data.user);
         setUsername(response.data.user.username);
-        socket.emit("register", response.data.user._id);
+        
+        await socket.emit("register", sender);
         setMessages(response.data.user.messages);
-        console.log(messages);
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
@@ -42,34 +46,39 @@ function User() {
 
     fetchData();
 
-    // Listen for incoming messages
-    socket.on("message", (newMessage) => {
-      setMessages((prevMessages) => [...prevMessages, newMessage]);
+    socket.on("msg", (messageData) => {
+      console.log("New message received:", messageData);
+      setMessages((prevMessages) => [...prevMessages, messageData]);
     });
 
-    // Cleanup on unmount
     return () => {
-      socket.off("message");
       socket.disconnect();
     };
   }, [userId]);
 
-  useEffect(()=>{
-    const fetchData=async()=>{
-      const response2=await axios.get(`http://localhost:3000/messages/${userId}`,{withCredentials:true});
-    console.log(response2);
-    setMessages(response2.data.messages)
-    }
-   fetchData();
-  },[])
+  useEffect(() => {
+    const fetchData = async () => {
+      const response2 = await axios.get(
+        `http://localhost:3000/messages/${userId}`,
+        { withCredentials: true }
+      );
+      console.log(response2);
+      setMessages(response2.data.messages);
+    };
+    fetchData();
+  }, [userId]);
+
   const sendMessage = () => {
     if (message && username) {
-      socket.emit('message', {
+      const messageData = {
         from: sender,
         to: username,
-        message: message
-      });
-      setMessage('');
+        message: message,
+      };
+      console.log(messageData.from, messageData.to, messageData.message);
+      socket.emit("message", messageData);
+      setMessages((prevMessages) => [...prevMessages, messageData]);
+      setMessage("");
     }
   };
 
--- Server/server.js
+++ Server/server.js
@@ -5,11 +5,11 @@ const cors = require("cors");
 const http = require("http");
 const socketIo = require("socket.io");
 const cookieParser = require("cookie-parser");
-const jwt=require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const { timeStamp } = require("console");
 const PORT = 3000;
 const server = http.createServer(app);
-
+const users = {};
 const secretKey = "Se3c4r4e4tk4e0y";
 require("dotenv").config();
 app.use(express.json());
@@ -31,9 +31,9 @@ const io = socketIo(server, {
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  socketId: String,
   messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
 });
+
 const messageSchema = new mongoose.Schema({
   from: String,
   to: String,
@@ -43,6 +43,7 @@ const messageSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 const Message = mongoose.model("Message", messageSchema);
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -57,34 +58,50 @@ mongoose
 
 io.on("connection", (socket) => {
   console.log("A user connected: " + socket.id);
+
   socket.on("register", async (username) => {
     try {
+      console.log("username", username);
       const user = await User.findOne({ username });
       if (user) {
-        user.socketId = socket.id;
-        await user.save();
+        console.log("Hii");
+        users[username] = socket.id;
       }
     } catch (error) {
       console.error("Error registering user:", error);
     }
   });
 
-  socket.on("message", async ({ from, to, message }) => {
+  socket.on("message", async (messageData) => {
     try {
-      const newMessage = new Message({ from, to, message });
-      await newMessage.save();
-    
-      const recipient = await User.findOne({ username: to });
-      const user=await User.findOne({username:from});
-      console.log(user);
-      user.messages.push(newMessage);
-      recipient.messages.push(newMessage);
-      await user.save();
-      await recipient.save();
-      if (recipient && recipient.socketId) {
-        io.to(recipient.socketId).emit("message", newMessage);
-
+      let receiver = users[messageData.to];
+      console.log("receiver", users[messageData.to], users);
+      if (receiver) {
+        io.to(receiver).emit("msg", messageData);
       }
+
+      const newMessage = new Message(messageData);
+      await newMessage.save();
+
+      const senderUser = await User.findOne({ username: messageData.from });
+      const recipientUser = await User.findOne({ username: messageData.to });
+
+      if (senderUser && recipientUser) {
+        senderUser.messages.push(newMessage);
+        recipientUser.messages.push(newMessage);
+        await senderUser.save();
+        await recipientUser.save();
+      }
+
+      socket.on('disconnect', () => {
+        console.log('A user disconnected: ' + socket.id);
+        for (const [username, socketId] of Object.entries(users)) {
+          if (username === username) {
+            delete users[username];
+            break;
+          }
+        }
+      });
     } catch (error) {
       console.error("Error sending message:", error);
     }
@@ -111,8 +128,6 @@ const auth = (req, res, next) => {
   }
 };
 
-
-
 app.post("/signup", async (req, res) => {
   const { username, password } = req.body;
   try {
@@ -136,9 +151,9 @@ app.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username, password });
     if (user) {
-      const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+      const token = jwt.sign({ username }, secretKey, { expiresIn: "1h" });
       res.cookie("token", token, { httpOnly: true, maxAge: 86400000 });
-      res.status(200).json({ message: "Logged in successfully",token });
+      res.status(200).json({ message: "Logged in successfully", token });
     } else {
       res.status(404).json({ message: "Wrong credentials" });
     }
@@ -162,7 +177,7 @@ app.get("/users", auth, async (req, res) => {
 app.get("/users/:userId", auth, async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.params.userId });
-    console.log(user);
+    //console.log(user);
     if (user) {
       res.json({ user });
     } else {
@@ -174,29 +189,24 @@ app.get("/users/:userId", auth, async (req, res) => {
   }
 });
 
-app.get("/messages/:userId",auth,async(req,res)=>{
-  try{
-    const user=await User.findOne({_id:req.params.userId});
+app.get("/messages/:userId", auth, async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.userId });
     //console.log("USer is",user);
-    const messages=await Message.find({  $or: [
-      {
-        $and: [
-          { to: user.username },
-          { from: req.user.username }
-        ]
-      },
-      {
-        $and: [
-          { to: req.user.username },
-          { from: user.username }
-        ]
-      }
-    ]}).sort({timeStamp:1});
+    const messages = await Message.find({
+      $or: [
+        {
+          $and: [{ to: user.username }, { from: req.user.username }],
+        },
+        {
+          $and: [{ to: req.user.username }, { from: user.username }],
+        },
+      ],
+    }).sort({ timeStamp: 1 });
     console.log(req.user.username);
-    res.send({messages});
+    res.send({ messages });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
-  catch(error){
-    console.error("Error fetching user:",error);
-    res.status(500).json({message:"Internal Server Error"});
-  }
-})
+});
