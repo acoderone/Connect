61bbbae7f79ffc2570b888481da5ebe0b521080a
Acoderone <avi5200786@gmail.com>
2024-07-09 07:19:48 +0530

    socket problem

--- Client/Connect/src/Components/User.jsx
+++ Client/Connect/src/Components/User.jsx
@@ -6,6 +6,8 @@ import {jwtDecode} from "jwt-decode";
 
 const socket = io("http://localhost:3000", {
   withCredentials: true,
+  transports: ['websocket'], // Ensure WebSocket transport is used
+  autoConnect: true,
 });
 
 function User() {
@@ -16,12 +18,9 @@ function User() {
   const [username, setUsername] = useState("");
   const [sender, setSender] = useState("");
 
-  useEffect(()=>{
-    socket.emit("register", sender);
-  },[sender])
   useEffect(() => {
-    socket.connect();
-     
+    socket.connect(); // Connect the socket
+
     const token = localStorage.getItem("token");
     if (token) {
       const decodedToken = jwtDecode(token);
@@ -39,13 +38,12 @@ function User() {
         console.log(response);
         setUser(response.data.user);
         setUsername(response.data.user.username);
-        
-       
-        //setMessages(response.data.user.messages);
+        setMessages(response.data.user.messages); // Fetch initial messages
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
     };
+
     const fetchMessages = async () => {
       const response2 = await axios.get(
         `http://localhost:3000/messages/${userId}`,
@@ -54,6 +52,7 @@ function User() {
       console.log(response2);
       setMessages(response2.data.messages);
     };
+
     fetchData();
     fetchMessages();
 
@@ -63,12 +62,16 @@ function User() {
     });
 
     return () => {
-      socket.disconnect();
+      socket.off("msg");
+      socket.disconnect(); // Clean up the socket connection
     };
   }, [userId]);
 
- 
-  
+  useEffect(() => {
+    if (sender) {
+      socket.emit("register", sender); // Emit register event when sender is set
+    }
+  }, [sender]);
 
   const sendMessage = () => {
     if (message && username) {
@@ -78,7 +81,7 @@ function User() {
         message: message,
       };
       console.log(messageData.from, messageData.to, messageData.message);
-      socket.emit("message", messageData);
+      socket.emit("message", messageData); // Emit message event
       setMessages((prevMessages) => [...prevMessages, messageData]);
       setMessage("");
     }
